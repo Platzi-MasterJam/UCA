f3057c05bb6cb9e77cbcb7c45972356a347e4bb1
Carlos <carlos.gutierrez@tortoise.dev>
2021-06-12 14:18:19 -0500

    dockerizing the app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+  const port = 4000;
   await app.listen(port);
 }
 bootstrap();
