ecbcb2d2bd74a1bde255ac20aa17fcf1917e0d75
Carlos <carlos.gutierrez@tortoise.dev>
2021-06-12 20:25:19 -0500

    adding new script 2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 4000;
+  const port = process.env.PORT || 3000;
   app.enableCors()
   await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
